be2a923e249c0207e7ea823cbeeabd04cac71e7d
Yash708262 <yash708262@gmail.com>
2025-09-19 14:33:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
             // Get the radius from the user using the prompt() function
             let radius = prompt("Enter the radius of the circle:");
             // write you code here and display the result to the user
-           
+           let area = Math.PI * radius ** 2;
+	 let areaTwoDecimals = area.toFixed(2); // 2 decimal places
+	 return areaTwoDecimals;
 }
 calculateArea();
